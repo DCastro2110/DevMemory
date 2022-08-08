4373cc7539436ec26bdb36d761d313a017e90a09
Davi Castro <santos.davi2110@gmail.com>
2022-08-08 17:42:21 -0300

    vertical align of radio inputs

--- src/components/RadioInput/style.ts
+++ src/components/RadioInput/style.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const Radio = styled.label`
-  margin-right: 10px;
+  gap: 10px;
   font-size: 16px;
-  
+  display: flex;
+  align-items: center;
+
   input {
     margin-left: 5px;
   }
