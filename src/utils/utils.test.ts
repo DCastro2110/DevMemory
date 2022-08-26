c31457f6e26880cbc947aa3b9a2c8c5d32008775
Davi Castro <santos.davi2110@gmail.com>
2022-08-26 08:57:34 -0300

    test: Test of the utils was edited

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,19 +1,22 @@
 import { randomNum } from "./randomNum";
-import { timerFormator } from './timerFormator'
+import { timerFormator } from "./timerFormator";
 
-it('should return a integer less than or equal the double of quant informated', () => {
-  let result = randomNum(10);
+it("should return a integer less than or equal the double of quant informated", () => {
+  let result1 = randomNum(10);
+  let result2 = randomNum(45);
 
-  expect(result).toBeLessThanOrEqual(20);
-})
+  expect(result1).toBeLessThan(20);
+  expect(result2).toBeLessThan(45);
+});
 
 it("should transform secons in hours formate", () => {
   let seconds1 = 3400;
-  let seconds2 =  2435;
+  let seconds2 = 2435;
+  let seconds3 = 3600;
 
   expect(timerFormator(seconds1)).toBe("56:40");
   expect(timerFormator(seconds2)).toBe("40:35");
-
-})
+  expect(timerFormator(seconds3)).toBe("00:00");
+});
 
 export {};
